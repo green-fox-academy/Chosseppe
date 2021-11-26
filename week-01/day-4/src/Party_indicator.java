2716e963610be06fbe9573b58eba4cdf47b96857
choseppe <pruder.chose@icloud.com>
2021-11-26 11:54:59 +0100

    Finished one sketch started yesterday

--- week-01/day-4/src/Party_indicator.java
+++ week-01/day-4/src/Party_indicator.java
@@ -13,16 +13,21 @@ public class Party_indicator {
 
         // If the the number of girls and boys are equal and 20 or more people are coming to the party
         // it should print: The party is excellent!
-        //
+        if (Input + Input_2 >= 20)
+            if (Input == Input_2)
+                System.out.println("The party si Excellent!");
         // If there are 20 or more people coming to the party but the girl - boy ratio is not 1-1
         // it should print: Quite a cool party!
-        //
+            else
+                System.out.println("Quite a cool party!");
         // If there are less people coming than 20
         // it should print: Average party...
-        //
+        else
+            System.out.println("Average party...");
         // If no girls are coming, regardless the count of the people
         // it should print: Sausage party
-        
+        if (Input == 0)
+            System.out.println("Sausage party");
 
     }
 }
