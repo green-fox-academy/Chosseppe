9f347f02f936950f6a8f14e2a85bcdfab64522c8
choseppe <pruder.chose@icloud.com>
2021-12-29 16:41:08 +0100

    finished exercise Doubled

--- week-06/day-3/src/Doubled.java
+++ week-06/day-3/src/Doubled.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class Doubled {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         // Create a method called decryptDoubled() that takes a filename as string as a parameter
         // and it can decrypt the duplicated-chars.txt file
         // Decryption is the process reversing an encryption, i.e. the process
@@ -15,7 +15,7 @@ public class Doubled {
         // If the file can't be opened it should return this message: File not found
         // The result should be saved in the output.txt file
 
-        readFile("Duplicated.txt");
+        decryptDoubled(readFile("src/duplicated-chars.txt"));
     }
 
     private static List<String> readFile(String inputPath){
@@ -24,20 +24,33 @@ public class Doubled {
         try{
             lines = Files.readAllLines(filePath);
         }catch (IOException e){
-            System.err.println("Unable to read file on path: " + inputPath);
+            System.err.println("File not found");
         }
         return lines;
     }
 
-    /*private static void decryptDoubled(List<String> inputList) throws IOException {
+    private static void decryptDoubled(List<String> inputList){
 
 
         List<String> output = new ArrayList<>();
-        String temp;
+        StringBuilder temp = new StringBuilder();
+
         for(String line : inputList){
-            System.out.println(line);
+            new StringBuilder(line);
+            for(int i = 1; i <= line.length(); i++){
+                if(i % 2 == 0){
+                    temp.append(line.charAt(i - 1));
+                }
+            }
+            output.add(temp.toString());
+            temp = new StringBuilder();
+        }
+        try {
+            Files.write(Path.of("src/output.txt"), output);
+        }catch (IOException e){
+            System.out.println("File not saved");
         }
     }
 
-     */
+
 }
