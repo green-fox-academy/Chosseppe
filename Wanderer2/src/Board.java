607dbb0c17ed669778af49a0f40c4f8506772b2c
choseppe <pruder.chose@icloud.com>
2022-01-04 23:46:02 +0100

    completed camban task 10

--- Wanderer2/src/Board.java
+++ Wanderer2/src/Board.java
@@ -10,23 +10,53 @@ public class Board extends JComponent implements KeyListener {
     int boxX;
     int boxY;
 
+    Level level = new Level();
+
     public Board() {
         boxX = 0;
         boxY = 0;
 
         // set the size of your draw board
-        setPreferredSize(new Dimension(800, 660));
+        setPreferredSize(new Dimension(1080, 720));
         setVisible(true);
     }
 
     @Override
     public void paint(Graphics graphics) {
         super.paint(graphics);
-        //graphics.fillRect(testBoxX, testBoxY, 60, 60);
+        graphics.fillRect(720, 0, 920, 720);
         // here you have a 720x720 canvas
         // you can create and draw an image using the class below e.g.
-        PositionedImage image = new PositionedImage("wanderer-java/img/hero-" + position + ".png", boxX, boxY);
-        image.draw(graphics);
+        for(int x = 0; x < 10; x ++) {
+            for (int y = 0; y < 10; y ++) {
+                if(level.gets(level.get(y), x).equals(1)){
+                    PositionedImage wallImage = new
+                            PositionedImage("wanderer-java/img/wall.png", x * 72, y * 72);
+                    wallImage.draw(graphics);
+                }else {
+                    PositionedImage groundImage = new
+                            PositionedImage("wanderer-java/img/floor.png", x * 72, y * 72);
+                    groundImage.draw(graphics);
+                }
+            }
+        }
+        for(int x = 0; x < 10; x ++) {
+            for (int y = 0; y < 10; y ++) {
+                if (level.gets(level.get(y), x).equals('S')) {
+                    PositionedImage skeletonImage = new
+                            PositionedImage("wanderer-java/img/skeleton.png", x * 72, y * 72);
+                    skeletonImage.draw(graphics);
+                } else if (level.gets(level.get(y), x).equals('B')) {
+                    PositionedImage bossImage = new
+                            PositionedImage("wanderer-java/img/boss.png", x * 72, y * 72);
+                    bossImage.draw(graphics);
+                }
+                PositionedImage image = new
+                        PositionedImage("wanderer-java/img/hero-" + position + ".png", boxX * 72, boxY * 72);
+                image.draw(graphics);
+
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -61,37 +91,54 @@ public class Board extends JComponent implements KeyListener {
     public void keyReleased(KeyEvent e) {
         // When the up or down keys hit, we change the position of our box
         if (e.getKeyCode() == KeyEvent.VK_UP) {
-            if(boxY == 0){
+            if (boxY == 0) {
                 position = "up";
-            }else {
-                boxY -= 60;
-                position = "up";
+            } else {
+                if(!level.gets(level.get(boxY - 1), boxX).equals(1) && boxY - 1 >= 0) {
+                    boxY--;
+                    position = "up";
+                }else{
+                    position = "up";
+                }
             }
-        } else if(e.getKeyCode() == KeyEvent.VK_DOWN) {
-            if(boxY == 600){
+        } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+            if (boxY == 9) {
                 position = "down";
-            }else {
-                boxY += 60;
-                position = "down";
+            } else {
+                if(!level.gets(level.get(boxY + 1), boxX).equals(1) && boxY + 1 <= 9) {
+                    boxY++;
+                    position = "down";
+                }else {
+                    position = "down";
+                }
             }
-        } else if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
-            if(boxX == 600){
+        } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            if (boxX == 9) {
                 position = "right";
-            }else {
-                boxX += 60;
-                position = "right";
+            } else {
+                if(!level.gets(level.get(boxY), boxX + 1).equals(1) && boxX + 1 <= 9) {
+                    boxX++;
+                    position = "right";
+                }else {
+                    position = "right";
+                }
             }
-        } else if(e.getKeyCode() == KeyEvent.VK_LEFT) {
-            if(boxX == 0){
+        } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            if (boxX == 0) {
                 position = "left";
-            }else {
-                boxX -= 60;
-                position = "left";
+            } else {
+                if(!level.gets(level.get(boxY), boxX - 1).equals(1) && boxX - 1 >= 0) {
+                    boxX--;
+                    position = "left";
+                }else {
+                    position = "left";
+                }
             }
+        } else if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+            System.out.println("Blob");
         }
         // and redraw to have a new picture with the new coordinates
         repaint();
 
     }
-
 }
---
+++ Wanderer2/src/Level.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Level {
+
+
+    List<List<Object>> level;
+
+    public Level() {
+
+        level= new ArrayList<>();
+        List<Object> x0 = Arrays.asList(0, 0, 'X', 1, 0, 1, 0, 0, 0, 0);
+        level.add(x0);
+        List<Object> x1 = Arrays.asList(0, 0, 0, 1, 0, 1, 0, 1, 1, 0);
+        level.add(x1);
+        List<Object> x2 = Arrays.asList(0, 1, 1, 1, 0, 1, 0, 1, 1, 0);
+        level.add(x2);
+        List<Object> x3 = Arrays.asList(0, 0, 0, 0, 0, 1, 0, 0, 0, 0);
+        level.add(x3);
+        List<Object> x4 = Arrays.asList(1, 1, 1, 1, 0, 1, 1, 1, 1, 0);
+        level.add(x4);
+        List<Object> x5 = Arrays.asList('S', 1, 'S', 1, 0, 0, 0, 0, 1, 0);
+        level.add(x5);
+        List<Object> x6 = Arrays.asList(0, 1, 0, 1, 0, 1, 1, 0, 1, 0);
+        level.add(x6);
+        List<Object> x7 = Arrays.asList(0, 0, 0, 0, 0, 1, 1, 0, 1, 0);
+        level.add(x7);
+        List<Object> x8 = Arrays.asList(0, 1, 1, 1, 0, 0, 0, 0, 1, 0);
+        level.add(x8);
+        List<Object> x9 = Arrays.asList(0, 0, 'B', 1, 0, 1, 1, 'S', 1, 0);
+        level.add(x9);
+
+    }
+
+    public List<Object> get(int number) {
+        List<Object> row = level.get(number);
+        return row;
+    }
+
+    public Object gets(List<Object> row , int number){
+        Object object = row.get(number);
+        return object;
+    }
+}
