3258e24043ec986a94604b4703f7e162362155ac
choseppe <pruder.chose@icloud.com>
2022-01-31 15:10:49 +0100

    finished exercise Coloring around

--- week-08/day-2/hello-DI-Project/src/main/java/com/example/hellodiproject/color/RedColor.java
+++ week-08/day-2/hello-DI-Project/src/main/java/com/example/hellodiproject/color/RedColor.java
@@ -1,14 +1,24 @@
 package com.example.hellodiproject.color;
 
+import com.example.hellodiproject.helloWorld.Printer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
-//@Service
+@Service
 //@Primary
 public class RedColor implements MyColor {
 
+    @Autowired
+    private Printer myPrinter;
+
+    public RedColor(Printer myPrinter) {
+        this.myPrinter = myPrinter;
+    }
+
     public void printColor(){
-        System.out.println("\033[0;31m" + "It is red in color..." + "\033[0m");
+        //System.out.println("\033[0;31m" + "It is red in color..." + "\033[0m");
+        myPrinter.log("\033[0;31m It is red in color... \033[0m");
     }
 
 }
