e08cc7fcecfdb511d0010cb4c281072dc92b18fb
choseppe <pruder.chose@icloud.com>
2022-02-05 00:58:45 +0100

    finished almost all exercise aimed on thymeleaf

---
+++ week-07/day-4/src/main/java/com/thymeleaf/thymeleafhelloworld/ThymeleafHelloWorldApplication.java
@@ -0,0 +1,15 @@
+package com.thymeleaf.thymeleafhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafHelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafHelloWorldApplication.class, args);
+    }
+
+
+
+}
