026079d2846c0a57d4ce7f41b98767d9dcddbcae
choseppe <pruder.chose@icloud.com>
2022-05-15 23:23:43 +0200

    working on loging of fox in Programmer Fox Club exercise

---
+++ Eucyon/week-08/day-4_5/src/main/java/com/greenfoxaccademy/programmerfoxclub/controllers/LoginController.java
@@ -0,0 +1,15 @@
+package com.greenfoxaccademy.programmerfoxclub.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+
+    @RequestMapping(value = "/login")
+    public String homePageShow(){
+        return "login";
+    }
+}
