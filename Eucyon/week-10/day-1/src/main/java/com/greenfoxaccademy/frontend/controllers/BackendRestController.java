6e8c7d900c1af32b1f4b4e9fd9dbcb2b443d94ca
choseppe <pruder.chose@icloud.com>
2022-05-24 23:50:40 +0200

    finished exercise AppendA

--- Eucyon/week-10/day-1/src/main/java/com/greenfoxaccademy/frontend/controllers/BackendRestController.java
+++ Eucyon/week-10/day-1/src/main/java/com/greenfoxaccademy/frontend/controllers/BackendRestController.java
@@ -4,10 +4,7 @@ import com.greenfoxaccademy.frontend.models.Doubling;
 import com.greenfoxaccademy.frontend.models.ErrorMessage;
 import com.greenfoxaccademy.frontend.models.Greetings;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class BackendRestController {
@@ -24,23 +21,29 @@ public class BackendRestController {
         }
     }
 
-    @RequestMapping({"/greeter", "/greeter/"})
+    @RequestMapping(value ="/greeter")
     public ResponseEntity greetings(@RequestParam(name = "name", required = false) String name,
                                     @RequestParam(name = "title", required = false) String title){
         if(name == null && title == null){
             return ResponseEntity.status(200)
-                    .body("{\"error\": \"Please provide a name and a title!\"}");
+                    .body(new ErrorMessage("Please provide a name and a title!"));
         }else if(name == null){
             return ResponseEntity.status(200)
-                    .body("{\"error\": \"Please provide a name!\"}");
+                    .body(new ErrorMessage("Please provide a name!"));
         }else if(title == null){
             return ResponseEntity.status(200)
-                    .body("{\"error\": \"Please provide a title!\"}");
+                    .body(new ErrorMessage("Please provide a title!"));
         }else{
             Greetings greet = new Greetings(name,title);
             return ResponseEntity.status(200)
                     .body("{\"welcome_message\": \"Oh, hi there " + greet.getName() + ", my dear "
                             + greet.getTitle() + "!\"}");
         }
+    }
+
+    @GetMapping(value ="/appenda/{appendable}")
+    public ResponseEntity appendA(@PathVariable(name = "appendable") String append) {
+        return ResponseEntity.status(200).body("{\"appended\": \" " + append + "a\"}");
+
     }
 }
